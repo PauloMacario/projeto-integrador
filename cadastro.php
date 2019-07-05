70768f0ff2fc030dd7d9bf0208f23e0398824411
bia <BrunoCata>
2019-07-05 19:35:06 -0300

    Alterações na tela de cadastro e login

--- cadastro.php
+++ cadastro.php
@@ -10,21 +10,22 @@
         <div class="cadastro">
         <form action="" method="POST">
         <div class="form-group">
-            <label>Login</label>
-            <input type="text" class="form-control" id="login" placeholder=" Digite seu login de acesso" name="login">
+            <label>Digite seu Login</label>
+            <input type="text" class="form-control" id="login" placeholder=" Digite seu login de acesso" name="login" required>
         </div>
         <div class="form-group">
-            <label for="exampleInputPassword1">Senha</label>
-            <input type="password" class="form-control" id="senha" placeholder="Digite sua senha" name="senha">
+            <label for="exampleInputPassword1">Crie sua senha Senha</label>
+            <input type="password" class="form-control" id="senha" placeholder="Digite sua senha" name="senha" required>
         </div>
         <div class="form-group">
-            <label for="exampleInputPassword1">Nome</label>
-            <input type="text" class="form-control" id="nome" placeholder="Digite seu nome" name="nome">
+            <label for="exampleInputPassword1">Confirme sua senha Senha</label>
+            <input type="password" class="form-control" id="senha" placeholder="Digite sua senha" name="senha" required>
         </div>
         <div class="form-group">
-            <label for="exampleInputPassword1">Salario</label>
-            <input type="text" class="form-control" id="salario" placeholder="Digite seu salario" name="salario">
+            <label for="exampleInputPassword1">Digite seu nome Nome</label>
+            <input type="text" class="form-control" id="nome" placeholder="Digite seu nome" name="nome" required>
         </div>
+            <button type="submit" href="login.php" class="btn btn-primary">Voltar</button>
             <button type="submit" class="btn btn-primary">Cadastrar</button>
         </form>
         </div>
--- login.php
+++ login.php
@@ -11,11 +11,11 @@
         <form action="home.html" method="POST">
         <div class="form-group">
             <label>Login</label>
-            <input type="text" class="form-control" id="login" placeholder="Login" name="login">
+            <input type="text" class="form-control" id="login" placeholder="Login" name="login" required>
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Senha</label>
-            <input type="password" class="form-control" id="senha" placeholder="Senha" name="senha">
+            <input type="password" class="form-control" id="senha" placeholder="Senha" name="senha" required>
         </div>
             <button type="submit" class="btn btn-primary">Login</button>
             <a href="cadastro.php" class="btn btn-primary">Cadastrar</a> 
