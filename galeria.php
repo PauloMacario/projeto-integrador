5334b5ea8c3f7ebddc2014a06b279c7f6732430e
PAULO MACARIO <paulomacario14@gmail.com>
2019-07-18 16:17:00 -0300

    arquivo galeria responsivo

---
+++ galeria.php
@@ -0,0 +1,97 @@
+<?php
+require_once("inc/header.php");
+?>
+<div class="home">
+    <div class="overlay"></div>
+    <div class="home-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url(images/cover_bg_1.jpg);">
+        <div class="desc animate-box">
+            <h2>Galeria de <strong>Imagens</strong></h2>
+            <span>Algumas fotos da ações sociais<a href="" target="_blank" class="fh5co-site-name"></a></span>
+
+        </div>
+    </div>
+
+</div>
+<div id="projeto" class="projeto-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-md-offset-12 text-center heading-section animate-box">
+						<h3>Galeria de Fotos</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit est facilis maiores, perspiciatis accusamus asperiores sint consequuntur debitis.</p>
+					</div>
+				</div>
+			</div>
+		</div>	
+
+<div class="row row-bottom-padded-md">
+    <div class="col-12 ">
+        <ul id="fh5co-portfolio-list">
+
+            <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/cover_bg_1.jpg); ">
+                <a href="#" class="color-3">
+                    <div class="case-studies-summary">
+                        <span>Ex: 1</span>
+                        <h2>Título 1</h2>
+                    </div>
+                </a>
+            </li>
+            <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/cover_bg_1.jpg); ">
+                <a href="#" class="color-3">
+                    <div class="case-studies-summary">
+                        <span>Ex: 2</span>
+                        <h2>Título 2</h2>
+                    </div>
+                </a>
+            </li>
+            <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/cover_bg_3.jpg); ">
+                <a href="#" class="color-4">
+                    <div class="case-studies-summary">
+                        <span>Ex: 3</span>
+                        <h2>Título 3</h2>
+                    </div>
+                </a>
+            </li>
+
+            <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/cover_bg_1.jpg); ">
+                <a href="#" class="color-5">
+                    <div class="case-studies-summary">
+                        <span>Ex: 4</span>
+                        <h2>Título 4</h2>
+                    </div>
+                </a>
+            </li>
+            <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/cover_bg_3.jpg); ">
+                <a href="#" class="color-6">
+                    <div class="case-studies-summary">
+                        <span>Ex: 5</span>
+                        <h2>Título 5</h2>
+                    </div>
+                </a>
+            </li>
+            <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/cover_bg_1.jpg); ">
+                <a href="#" class="color-3">
+                    <div class="case-studies-summary">
+                        <span>Ex: 6</span>
+                        <h2>Título 6</h2>
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-4 col-md-offset-4 text-center animate-box">
+
+    </div>
+</div>
+
+
+</div>
+</div>
+
+
+
+<?php
+require_once("inc/footer.php");
+?>
