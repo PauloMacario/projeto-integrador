29015b56f6e3de3ea5c01f2eb546b5f38456737b
PAULO MACARIO <paulomacario14@gmail.com>
2019-07-18 16:19:19 -0300

    home - retirado footer e header (arquivos separados)

---
+++ home.php
@@ -0,0 +1,186 @@
+<?php
+
+	require_once("inc/header.php");
+
+?>
+		<a href="#" target="_blank" class=""> <!-- fh5co-site-name -->
+		<div class="home" id="home">
+					<div class="overlay"></div>
+			
+					<div class="home-cover text-center" data-stellar-background-ratio="1" style="background-image: url(images/cover_nature_01.jpg);background-size: cover; background-repeat: no-repeat;">
+						<div class="desc animate-box">
+							<h2><strong>Seja voluntário!</strong></h2>
+							<span>Encontre aqui um trabalho voluntário para participar </span>
+							<button type="button" class="btn btn-success">Comece já</button>
+						</div>
+					</div>
+	
+		
+		</div>
+	</a>
+
+		
+
+		<div id="projeto" class="projeto-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center heading-section animate-box">
+						<h3>Encontre sua causa</h3>
+						<p>Clique numa causa e encontre organizações em São Paulo.</p>
+					</div>
+				</div>
+
+				<div class="row row-bottom-padded-md">
+					<div class="col-lg-4 col-md-12 col-sm-12 text-center animate-box">
+						<div class="projeto-img">	
+							<p><img src="images/cover_children_1.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive img-large"></p>
+						</div>
+						<div class="projeto-text">
+							<h3>Criança e Adolescente</h3>
+							<p>Encontre aqui organizações que ajudam em orfanatos, creches, hospitais e lazer com as crianças e adolescentes!</p>
+							<p><a href="#">Saiba mais</a></p>
+						</div>	
+					</div>
+					<div class="col-lg-4 col-md-12 col-sm-12 text-center animate-box">
+						<div class="projeto-img">	
+							<p><img src="images/cover_animals_2.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive img-large"></p>
+						</div>
+						<div class="projeto-text">
+							<h3>Proteção aos Animais</h3>
+							<p>Encontre aqui uma ação e seja um voluntário para auxílio em eventos de adoção de animais, veterinários ou formandos em veterinária para cuidados com os animais, etc.!</p>
+							<p><a href="#">Saiba mais</a></p>
+						</div>
+
+					</div>
+					<div class="col-lg-4 col-md-12 col-sm-12 text-center animate-box">
+						<div class="projeto-img">
+							<p><img src="images/cover_meio_3.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive img-large"></p>
+						</div>
+						<div class="projeto-text">
+							<h3>Meio Ambiente</h3>
+							<p>Todos nós sabemos que o planeta Terra não está bem e que precisa da ajuda de todos os cidadãos. Seja um voluntário para contribuir com as ações práticas de conservação do meio ambiente!<p>
+							<p><a href="#">Saiba mais</a></p>
+						</div>
+					</div>
+				
+
+				
+				</div>
+			</div>
+		</div>	
+		
+		
+
+				
+			
+		
+		<div id="busca-section" class="projeto-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12  text-center heading-section animate-box">
+						<h3>Busque aqui uma região para ajudar</h3>
+
+
+						<form>
+								<div class="form-row">
+									<div class="form-group col-md-10">
+										
+										<input type="email" class="form-control" id="inputEmail4" placeholder="Localizar região...">
+									</div>
+									<div class="form-check col-md-2">
+										<button type="submit" class="btn btn-success">Buscar</button>									
+									</div>
+								</div>
+						</form>
+
+					</div> 
+				</div>
+			</div>
+
+
+			<div class="container">
+				<div class="row">
+						<div class="col-md-12">
+						<div class="mapa text-center animate-box" style="height:80vh;" >
+							
+
+						
+							<p>Add Mapa do Google maps</p>
+
+						</div>
+					</div> 
+					
+				</div>
+			</div>
+		</div>
+		
+		<div id="noticia-section" class="projeto-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-md-offset-12 text-center heading-section animate-box">
+						<h3>Exemplo: Noticias ou Blog </h3>
+						<p>Lorem ipsum dolor sit amet, consectetur </p>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row row-bottom-padded-md">
+					<div class="col-lg-4 col-md-12 col-sm-12">
+						<div class="noticia animate-box">
+							 <iframe width="100%" height="280" src="https://www.youtube.com/embed/j8L1CcanjT8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3><a href=""#>Conheça os 17 Objetivos de Desenvolvimento Sustentável</a></h3>
+									<span class="posted_by">Sep. 15th</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Acesse aqui a Agenda 2030 na íntegra, com o documento final adotado na Cúpula das Nações Unidas sobre o Desenvolvimento Sustentável.</p>
+									<p><a href="#">Ler mais...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-12 col-sm-12">
+						<div class="noticia animate-box">
+							<a href="#"><img class="img-responsive" src="images/cover_bg_2.jpg" alt=""></a>
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3><a href=""#>Notícia 2</a></h3>
+									<span class="posted_by">Sep. 15th</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Breve descrição da notícia.</p>
+									<p><a href="#">Ler mais..</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="clearfix visible-sm-block"></div>
+					<div class="col-lg-4 col-md-12 col-sm-12">
+						<div class="noticia animate-box">
+							<a href="#"><img class="img-responsive" src="images/cover_bg_3.jpg" alt=""></a>
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3><a href=""#>Notícia 3</a></h3>
+									<span class="posted_by">Sep. 15th</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Breve descrição da notícia.</p>
+									<p><a href="#">Ler mais..</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="clearfix visible-md-block"></div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-12 col-md-12 text-center animate-box">
+						<a href="#" class="btn btn-success btn-lg">Mais Notícias</a>
+					</div>
+				</div>
+
+			</div>
+		</div>
+		<!-- noticia-section -->
+		
+<?php		
+	require_once("inc/footer.php");		
+?>	
