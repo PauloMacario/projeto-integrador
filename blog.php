578b114b67d976336523bf06b948b12798efd0e8
PAULO MACARIO <paulomacario14@gmail.com>
2019-07-18 16:15:54 -0300

    arquivo blog responsivo

---
+++ blog.php
@@ -0,0 +1,174 @@
+<?php
+	require_once("inc/header.php");
+?>
+
+		<div class="home">
+			<div class="overlay"></div>
+			<div class="home-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url(images/cover_bg_1.jpg);">
+				<div class="desc animate-box">
+					<h2>Our <strong>Blog &amp; News</strong></h2>
+					<span>HandCrafted by <a href="http://frehtml5.co/" target="_blank" class="fh5co-site-name">FreeHTML5.co</a></span>
+					<span><a class="btn btn-primary btn-lg" href="#">Donate Now</a></span>
+				</div>
+			</div>
+
+		</div>
+		
+		<div id="noticia" class="projeto-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-md-offset-12 text-center heading-section animate-box">
+						<h3>Read. Learn. Share</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit est facilis maiores, perspiciatis accusamus asperiores sint consequuntur debitis.</p>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row row-bottom-padded-md">
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="noticia animate-box">
+							<a href="#"><img class="img-responsive" src="images/cover_bg_1.jpg" alt=""></a>
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3><a href=""#>Medical Mission in Southern Kenya</a></h3>
+									<span class="posted_by">Sep. 15th</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="noticia animate-box">
+							<a href="#"><img class="img-responsive" src="images/cover_bg_2.jpg" alt=""></a>
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3><a href=""#>Medical Mission in Southern Kenya</a></h3>
+									<span class="posted_by">Sep. 15th</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="clearfix visible-sm-block"></div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="noticia animate-box">
+							<a href="#"><img class="img-responsive" src="images/cover_bg_3.jpg" alt=""></a>
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3><a href=""#>Medical Mission in Southern Kenya</a></h3>
+									<span class="posted_by">Sep. 15th</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="clearfix visible-md-block"></div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="noticia animate-box">
+							<a href="#"><img class="img-responsive" src="images/cover_bg_1.jpg" alt=""></a>
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3><a href=""#>Medical Mission in Southern Kenya</a></h3>
+									<span class="posted_by">Sep. 15th</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="clearfix visible-sm-block"></div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="noticia animate-box">
+							<a href="#"><img class="img-responsive" src="images/cover_bg_2.jpg" alt=""></a>
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3><a href=""#>Medical Mission in Southern Kenya</a></h3>
+									<span class="posted_by">Sep. 15th</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="noticia animate-box">
+							<a href="#"><img class="img-responsive" src="images/cover_bg_3.jpg" alt=""></a>
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3><a href=""#>Medical Mission in Southern Kenya</a></h3>
+									<span class="posted_by">Sep. 15th</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="clearfix visible-sm-block"></div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="noticia animate-box">
+							<a href="#"><img class="img-responsive" src="images/cover_bg_1.jpg" alt=""></a>
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3><a href=""#>Medical Mission in Southern Kenya</a></h3>
+									<span class="posted_by">Sep. 15th</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="clearfix visible-md-block"></div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="noticia animate-box">
+							<a href="#"><img class="img-responsive" src="images/cover_bg_2.jpg" alt=""></a>
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3><a href=""#>Medical Mission in Southern Kenya</a></h3>
+									<span class="posted_by">Sep. 15th</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="clearfix visible-sm-block"></div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="noticia animate-box">
+							<a href="#"><img class="img-responsive" src="images/cover_bg_3.jpg" alt=""></a>
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3><a href=""#>Medical Mission in Southern Kenya</a></h3>
+									<span class="posted_by">Sep. 15th</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-12 col-md-offset-12 text-center animate-box">
+						<a href="#" class="btn btn-primary btn-lg">Our Blog</a>
+					</div>
+				</div>
+
+			</div>
+		</div>
+		<!-- noticia -->
+
+<?php		
+	require_once("inc/footer.php");
+?>
