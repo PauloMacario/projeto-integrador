0de5fff36ab0d36f34c63dc3f768315ba75eef39
PAULO MACARIO <paulomacario14@gmail.com>
2019-07-18 16:20:55 -0300

    criado dir inc com arquivo footer e header

---
+++ inc/header.php
@@ -0,0 +1,130 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Nome_Projeto &mdash; Estenda sua mão a quem precisa</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Projeto Integrador DH" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'> -->
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Superfish -->
+	<link rel="stylesheet" href="css/superfish.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/login.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		<div id="box-principal">
+			<div id="page">
+				<div class="header-top">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-12 col-sm-12 text-right social">
+									<a href="https://www.facebook.com/" class="grow"><i class="icon-facebook2"></i></a>
+									<a href="https://twitter.com/" class="grow"><i class="icon-twitter2"></i></a>
+									<a href="https://www.instagram.com/" class="grow"><i class="icon-instagram2"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<header id="header-section" class="">
+						
+							<div class="nav-header">
+									<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+											<!-- <a class="navbar-brand" href="#">Navbar</a> -->
+											<form class="form-inline my-2 my-lg-0">
+													<input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+													<button class="btn btn-outline-primary my-2 my-sm-0" type="submit">
+														BUSCA
+													</button>
+												  </form>
+											<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+											  <span class="navbar-toggler-icon"></span>
+											</button>
+										  
+											<div class="collapse navbar-collapse menu-topo" id="conteudoNavbarSuportado">
+											  <ul class="navbar-nav mr-auto">
+												<li class="nav-item ">
+												  <a class="nav-link" href="home.php">Home </a>
+												</li>
+
+
+												<li class="nav-item">
+													<a class="nav-link" href="Projetos.php">Projetos</a>
+												</li>
+
+												<!-- <li class="nav-item dropdown">
+												  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+													Projetos
+												  </a>
+												  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+													<a class="dropdown-item" href="#">Criança e Adolescente</a>
+													<a class="dropdown-item" href="#">Proteção aos Animais</a>
+													<a class="dropdown-item" href="#">Meio Ambiente</a>
+													
+												  </div>
+												</li> -->
+												<li class="nav-item">
+													<a class="nav-link" href="galeria.php">Fotos</a>
+												</li>
+												<li class="nav-item">
+													<a class="nav-link" href="blog.php">Blog</a>
+												</li>
+												<li class="nav-item">
+													<a class="nav-link" href="Sobre.php">Sobre Nós</a>
+												</li>
+												<li class="nav-item">
+													<a class="nav-link" href="contato.php">Contato</a>
+												</li>
+												<li class="nav-item">
+													<a class="nav-link" href="login.php">Login</a>
+												</li>
+												
+											  </ul>
+											 
+											</div>
+										  </nav>
+							</div>
+						
+					</header>	
