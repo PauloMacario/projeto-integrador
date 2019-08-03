7cca8949e355a6efee5f37840f2e7f1d138bd9ef
PAULO MACARIO <paulomacario14@gmail.com>
2019-08-03 14:53:36 -0300

    Pequenas alterações

--- faq.php
+++ faq.php
@@ -5,85 +5,76 @@ require_once("inc/header.php");
 
 <div class="home">
 	<div id="faq" class="faq-section">
-		<div class="container">
-			<div class="accordion" id="accordionExample">
-				<div class="card">
-					<div class="card-header" id="headingOne">
-						<h2 class="mb-0">
-							<button class="btn btn-link btn-faq" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-								Quem somos?
-							</button>
-						</h2>
-					</div>
+			<div class="container container-faq">		
+				<div id="accordion">
 
-					<div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+					<div class="card">
+					<div class="card-header">
+					<a class="collapsed card-link link-faq" data-toggle="collapse" href="#collapseOne">
+					Quem somos?
+						</a>
+					</div>
+					<div id="collapseOne" class="collapse" data-parent="#accordion">
 						<div class="card-body">
 							Esta é uma plataforma onde pessoas podem encontrar facilmente uma instituição ou uma ação social em que esteja interessado em participar.
 						</div>
 					</div>
-				</div>
+					</div>
 
-				<div class="card">
-					<div class="card-header" id="headingTwo">
-						<h2 class="mb-0">
-							<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-								Qual o custo de participar?
-							</button>
-						</h2>
+					<div class="card">
+					<div class="card-header">
+						<a class="collapsed card-link " data-toggle="collapse" href="#collapseTwo">
+							Qual o custo de participar?
+						</a>
 					</div>
-					<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+					<div id="collapseTwo" class="collapse" data-parent="#accordion">
 						<div class="card-body">
-							A Páginas do Bem é uma plataforma gratuita e fica a critério do usuário a doação e a participação em instituições que solicitem alguma forma de contribuição financeira.
+						A Páginas do Bem é uma plataforma gratuita e fica a critério do usuário a doação e a participação em instituições que solicitem alguma forma de contribuição financeira.
 						</div>
 					</div>
-				</div>
-				<div class="card">
-					<div class="card-header" id="headingThree">
-						<h2 class="mb-0">
-							<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-								Posso participar em quantas ações?
-							</button>
-						</h2>
 					</div>
-					<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-						<div class="card-body">
-							Você é livre para participar em quantas ações quiser! Mas lembre-se de organizar sua agenda para não esquecer ou marcar duas ações no mesmo dia!
-						</div>
-					</div>
-				</div>
-				<div class="card">
-					<div class="card-header" id="headingFour">
-						<h2 class="mb-0">
-							<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-								Sou uma instituição e gostaria de divulgar
-							</button>
-						</h2>
-					</div>
-					<div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
-						<div class="card-body">
-							Estamos abertos a divulgar a sua página e/ou ação sem custo!
-							Basta clicar na opção <em>Sou instituição</em>.
-						</div>
-					</div>
-				</div>
 
-				<div class="card">
-					<div class="card-header" id="headingFive">
-						<h2 class="mb-0">
-							<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-								Pergunta #5
-							</button>
-						</h2>
+					<div class="card">
+					<div class="card-header">
+						<a class="collapsed card-link " data-toggle="collapse" href="#collapseThree">
+						Posso participar em quantas ações?
+						</a>
 					</div>
-					<div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
+					<div id="collapseThree" class="collapse" data-parent="#accordion">
 						<div class="card-body">
-							Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+						Você é livre para participar em quantas ações quiser! Mas lembre-se de organizar sua agenda para não esquecer ou marcar duas ações no mesmo dia!
 						</div>
 					</div>
-				</div>
+					</div>
+					<div class="card">
+					<div class="card-header">
+						<a class="collapsed card-link" data-toggle="collapse" href="#collapseFour">
+							<strong >Sou uma instituição e gostaria de divulgar
+						</a>
+					</div>
+					<div id="collapseFour" class="collapse" data-parent="#accordion">
+						<div class="card-body">
+						Estamos abertos a divulgar a sua página e/ou ação sem custo!
+												Basta clicar na opção <em>Sou instituição</em>.
+						</div>
+					</div>
+					</div>
+					<div class="card">
+					<div class="card-header">
+						<a class="collapsed card-link " data-toggle="collapse" href="#collapseFive">
+							Pergunta #5
+						</a>
+					</div>
+					<div id="collapseFive" class="collapse" data-parent="#accordion">
+						<div class="card-body">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+						</div>
+					</div>
+					</div>
+
+					</div>
 			</div>
-
-
+	
 		</div>
 	</div>
 </div>
