02d97285a267d15c066439eb45e68de70482b2d8
PAULO MACARIO <paulomacario14@gmail.com>
2019-08-06 02:50:50 -0300

    Require do head e header + identação do codigo e comentarios

--- faq.php
+++ faq.php
@@ -1,90 +1,93 @@
-<?php
-require_once("inc/header.php");
-?>
+
+<!-- ====== Cabeçalho HEAD (INÍCIO)  ======== -->
+<?php  require_once("inc/head.php"); ?>
 
 
-<div class="home">
-	<div id="faq" class="faq-section">
+<body>
+	<main id="box-principal">
+		<div id="page">
+
+ <!-- ====== Header e Menu de navegação (INÍCIO)  ========  -->
+<?php  require_once("inc/header.php"); ?>
+
+
+<!--  =========================== Seção Faq perguntas e respostas - (INÍCIO)  ==========================  -->
+	<section class="home">
+		<div id="faq" class="faq-section">
 			<div class="container container-faq">		
 				<div id="accordion">
+					<div class="card">
+						<div class="card-header">
+							<a class="collapsed card-link link-faq" data-toggle="collapse" href="#collapseOne">
+									Quem somos?
+							</a>
+						</div>
+						<div id="collapseOne" class="collapse" data-parent="#accordion">
+							<div class="card-body">
+								Esta é uma plataforma onde pessoas podem encontrar facilmente uma instituição ou uma ação social em que esteja interessado em participar.
+							</div>
+						</div>
+					</div>
+					<div class="card">
+						<div class="card-header">
+							<a class="collapsed card-link " data-toggle="collapse" href="#collapseTwo">
+									Qual o custo de participar?
+							</a>
+						</div>
+						<div id="collapseTwo" class="collapse" data-parent="#accordion">
+							<div class="card-body">
+									A Páginas do Bem é uma plataforma gratuita e fica a critério do usuário a doação e a participação em instituições que solicitem alguma forma de contribuição financeira.
+							</div>
+						</div>
+					</div>
 
 					<div class="card">
-					<div class="card-header">
-					<a class="collapsed card-link link-faq" data-toggle="collapse" href="#collapseOne">
-					Quem somos?
-						</a>
-					</div>
-					<div id="collapseOne" class="collapse" data-parent="#accordion">
-						<div class="card-body">
-							Esta é uma plataforma onde pessoas podem encontrar facilmente uma instituição ou uma ação social em que esteja interessado em participar.
+						<div class="card-header">
+							<a class="collapsed card-link " data-toggle="collapse" href="#collapseThree">
+							Posso participar em quantas ações?
+							</a>
+						</div>
+						<div id="collapseThree" class="collapse" data-parent="#accordion">
+							<div class="card-body">
+								Você é livre para participar em quantas ações quiser! Mas lembre-se de organizar sua agenda para não esquecer ou marcar duas ações no mesmo dia!
+							</div>
 						</div>
 					</div>
+					<div class="card">
+						<div class="card-header">
+							<a class="collapsed card-link" data-toggle="collapse" href="#collapseFour">
+								<strong >Sou uma instituição e gostaria de divulgar
+							</a>
+						</div>
+						<div id="collapseFour" class="collapse" data-parent="#accordion">
+							<div class="card-body">
+								Estamos abertos a divulgar a sua página e/ou ação sem custo! Basta clicar na opção <em>Sou instituição</em>.
+							</div>
+						</div>
+					</div>
+					<div class="card">
+						<div class="card-header">
+							<a class="collapsed card-link " data-toggle="collapse" href="#collapseFive">
+								Pergunta #5
+							</a>
+						</div>
+						<div id="collapseFive" class="collapse" data-parent="#accordion">
+							<div class="card-body">
+							Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+							</div>
+						</div>
 					</div>
 
-					<div class="card">
-					<div class="card-header">
-						<a class="collapsed card-link " data-toggle="collapse" href="#collapseTwo">
-							Qual o custo de participar?
-						</a>
-					</div>
-					<div id="collapseTwo" class="collapse" data-parent="#accordion">
-						<div class="card-body">
-						A Páginas do Bem é uma plataforma gratuita e fica a critério do usuário a doação e a participação em instituições que solicitem alguma forma de contribuição financeira.
-						</div>
-					</div>
-					</div>
-
-					<div class="card">
-					<div class="card-header">
-						<a class="collapsed card-link " data-toggle="collapse" href="#collapseThree">
-						Posso participar em quantas ações?
-						</a>
-					</div>
-					<div id="collapseThree" class="collapse" data-parent="#accordion">
-						<div class="card-body">
-						Você é livre para participar em quantas ações quiser! Mas lembre-se de organizar sua agenda para não esquecer ou marcar duas ações no mesmo dia!
-						</div>
-					</div>
-					</div>
-					<div class="card">
-					<div class="card-header">
-						<a class="collapsed card-link" data-toggle="collapse" href="#collapseFour">
-							<strong >Sou uma instituição e gostaria de divulgar
-						</a>
-					</div>
-					<div id="collapseFour" class="collapse" data-parent="#accordion">
-						<div class="card-body">
-						Estamos abertos a divulgar a sua página e/ou ação sem custo!
-												Basta clicar na opção <em>Sou instituição</em>.
-						</div>
-					</div>
-					</div>
-					<div class="card">
-					<div class="card-header">
-						<a class="collapsed card-link " data-toggle="collapse" href="#collapseFive">
-							Pergunta #5
-						</a>
-					</div>
-					<div id="collapseFive" class="collapse" data-parent="#accordion">
-						<div class="card-body">
-						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-						</div>
-					</div>
-					</div>
-
-					</div>
+				</div>
 			</div>
-	
 		</div>
-	</div>
-</div>
-</div>
+	</section>
+
+<!--  =========================== Seção Faq perguntas e respostas - (INÍCIO)  ==========================  -->
 
 
-
-
-
-
-<?php
-require_once("inc/footer.php");
+<!--  ======== Footer - (INÍCIO) ========  -->
+<?php		
+	require_once("inc/footer.php");
 ?>
+<!--  ======== Footer - (FINAL) ========  -->
