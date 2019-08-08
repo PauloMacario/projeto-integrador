35676468d6bce1a0567d0a5ff86814c0e87da9f5
Davi Seo <daviseo@Davis-MacBook-Pro.local>
2019-08-08 17:19:07 -0300

    criando nova pagina instituicoes

---
+++ instituicoes.php
@@ -0,0 +1,71 @@
+
+<!-- ====== Cabeçalho HEAD (INÍCIO)  ======== -->
+<?php  require_once("inc/head.php"); ?>
+
+
+<body>
+	<main id="box-principal">
+		<div id="page">
+
+ <!-- ====== Header e Menu de navegação (INÍCIO)  ========  -->
+<?php  require_once("inc/header.php"); ?>
+
+
+	<!--  =========================== Seção Instituições -  (INÍCIO)  ==========================  -->
+
+		<section id="noticia" class="projeto-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-md-offset-12 text-center heading-section animate-box">
+						<h3>Conheça as instituições que precisam da sua ajuda!</h3>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row row-bottom-padded-md">
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="noticia animate-box">
+							<a href="#"><img class="img-responsive" src="images/logo.png" alt="">
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3>ONG #1</h3>
+									<p>Logo e breve texto</p>
+								</div>
+                            </div>
+							</a>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="noticia animate-box">
+							<a href="#"><img class="img-responsive" src="images/logo.png" alt="">
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3>ONG #2</h3>
+									<p>Logo e breve texto</p>
+								</div>
+                            </div>
+                            </a>  
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="noticia animate-box">
+							<a href="#"><img class="img-responsive" src="images/logo.png" alt="">
+							<div class="noticia-text">
+								<div class="noticia-title">
+									<h3>ONG #3</h3>
+									<p>Logo e breve texto</p>
+								</div>
+                            </div>
+                            </a>  
+						</div>
+					</div>
+			</div>
+		</section>
+<!--  =========================== Seção Ações -  (FINAL)  ==========================  -->
+
+
+<!--  ======== Footer - (INÍCIO) ========  -->
+<?php		
+	require_once("inc/footer.php");
+?>
+<!--  ======== Footer - (FINAL) ========  -->
